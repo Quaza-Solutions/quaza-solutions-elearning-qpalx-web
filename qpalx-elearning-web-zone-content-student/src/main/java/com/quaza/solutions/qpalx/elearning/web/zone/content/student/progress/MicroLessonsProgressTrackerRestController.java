bb6f2b8122f60606bbfbaa174560ec65bfafe97e
manyce400 <fallon12>
2017-01-09 19:13:43 -0500

    Version 1.0 functionality:  Increasing size of Quiz Question feedback.  Adding dynamic loading of Quiz banner icons

---
+++ qpalx-elearning-web-zone-content-student/src/main/java/com/quaza/solutions/qpalx/elearning/web/zone/content/student/progress/MicroLessonsProgressTrackerRestController.java
@@ -0,0 +1,60 @@
+package com.quaza.solutions.qpalx.elearning.web.zone.content.student.progress;
+
+import com.quaza.solutions.qpalx.elearning.domain.lms.curriculum.QPalXEMicroLesson;
+import com.quaza.solutions.qpalx.elearning.domain.qpalxuser.QPalXUser;
+import com.quaza.solutions.qpalx.elearning.service.lms.adaptivelearning.statistics.IAdaptiveLessonStatisticsService;
+import com.quaza.solutions.qpalx.elearning.service.lms.curriculum.IQPalXEMicroLessonService;
+import com.quaza.solutions.qpalx.elearning.service.qpalxuser.IQPalxUserService;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * RestController that provides services for allow recording and tracking of MicroLessons completion progress for a
+ * QPalXUser
+ *
+ * @author manyce400
+ */
+@RestController
+public class MicroLessonsProgressTrackerRestController {
+
+
+
+
+    @Autowired
+    @Qualifier("quaza.solutions.qpalx.elearning.service.DefaultQPalxUserService")
+    private IQPalxUserService iqPalxUserService;
+
+    @Autowired
+    @Qualifier("quaza.solutions.qpalx.elearning.service.QPalXEMicroLessonService")
+    private IQPalXEMicroLessonService iqPalXEMicroLessonService;
+
+    @Autowired
+    @Qualifier("quaza.solutions.qpalx.elearning.service.AdaptiveLessonStatisticsService")
+    private IAdaptiveLessonStatisticsService iAdaptiveLessonStatisticsService;
+
+
+    private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(MicroLessonsProgressTrackerRestController.class);
+
+    @RequestMapping(value = "/micro-lesson-progress-tracker", method = RequestMethod.GET)
+    public void recordMicroLessonStatisticsEvent(@RequestParam("microLessonID") String microLessonID, @RequestParam("uniqueQPalxUserKey") String uniqueQPalxUserKey) {
+        LOGGER.info("Attempting to record MicroLesson with ID: {} progress for QPalXUser with email: {}", microLessonID, uniqueQPalxUserKey);
+
+        if(microLessonID != null && uniqueQPalxUserKey != null) {
+            Long id = NumberUtils.toLong(microLessonID);
+
+            // Locate the user and the QPalXEMicroLesson
+            QPalXUser qPalXUser = iqPalxUserService.findQPalXUserByEmail(uniqueQPalxUserKey);
+            QPalXEMicroLesson qPalXEMicroLesson = iqPalXEMicroLessonService.findByID(id);
+
+            // Record this as a micro lesson statistics event
+            LOGGER.info("Recording adaptive learning statistics event....");
+            iAdaptiveLessonStatisticsService.recordAdaptiveLessonStatistics(qPalXEMicroLesson, qPalXUser);
+        }
+    }
+
+}
---
+++ qpalx-elearning-web-zone-content-student/src/main/java/com/quaza/solutions/qpalx/elearning/web/zone/content/student/progress/QuestionBankProgressTrackerRestController.java
@@ -0,0 +1,58 @@
+package com.quaza.solutions.qpalx.elearning.web.zone.content.student.progress;
+
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.quaza.solutions.qpalx.elearning.domain.lms.adaptivelearning.scorable.QuestionBankItem;
+import com.quaza.solutions.qpalx.elearning.domain.qpalxuser.QPalXUser;
+import com.quaza.solutions.qpalx.elearning.service.lms.adaptivelearning.scorable.IQuestionBankService;
+import com.quaza.solutions.qpalx.elearning.service.qpalxuser.IQPalxUserService;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author manyce400
+ */
+@RestController
+public class QuestionBankProgressTrackerRestController {
+
+
+
+    @Autowired
+    @Qualifier("quaza.solutions.qpalx.elearning.service.DefaultQPalxUserService")
+    private IQPalxUserService iqPalxUserService;
+
+    @Autowired
+    @Qualifier("quaza.solutions.qpalx.elearning.service.QuestionBankService")
+    private IQuestionBankService iQuestionBankService;
+
+    @Autowired
+    @Qualifier("quaza.solutions.qpalx.elearning.service.StatisticsExecutorService")
+    private ListeningExecutorService listeningExecutorService;
+
+
+    private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(QuestionBankProgressTrackerRestController.class);
+
+
+    @RequestMapping(value = "/question-bank-progress-tracker", method = RequestMethod.GET)
+    public void recordQuestionBankStatisticsEvent(@RequestParam("questionBankItemID") String questionBankItemID, @RequestParam("uniqueQPalxUserKey") String uniqueQPalxUserKey) {
+        LOGGER.info("Attempting to record QuestionBankItem with ID: {} progress for QPalXUser with email: {}", questionBankItemID, uniqueQPalxUserKey);
+
+        Runnable statisticsRecordTask = () -> {
+            if (questionBankItemID != null && uniqueQPalxUserKey != null) {
+                Long id = NumberUtils.toLong(questionBankItemID);
+
+                // Get the qPalxUser for which metrics will be tracked as well as the questionBankItem
+                QPalXUser qPalXUser = iqPalxUserService.findQPalXUserByEmail(uniqueQPalxUserKey);
+                QuestionBankItem questionBankItem = iQuestionBankService.findByID(id);
+                iQuestionBankService.recordAdaptiveLessonStatistics(questionBankItem, qPalXUser);
+            }
+        };
+
+        listeningExecutorService.submit(statisticsRecordTask);
+    }
+
+}
