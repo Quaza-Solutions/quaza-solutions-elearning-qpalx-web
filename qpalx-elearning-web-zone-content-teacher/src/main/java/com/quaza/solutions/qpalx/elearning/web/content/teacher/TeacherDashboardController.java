d6e1401b53a9c52f38ce501516f4cf901b4a4db6
manyce400 <fallon12>
2017-05-07 14:36:22 -0400

    Version 1.0 functionality: Starting to put together homepage views for Executive, principal, teacher and parent/guardian users.

---
+++ qpalx-elearning-web-zone-content-teacher/src/main/java/com/quaza/solutions/qpalx/elearning/web/content/teacher/TeacherDashboardController.java
@@ -0,0 +1,17 @@
+package com.quaza.solutions.qpalx.elearning.web.content.teacher;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author manyce400
+ */
+@Controller
+public class TeacherDashboardController {
+
+
+
+    private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(TeacherDashboardController.class);
+
+
+
+}
