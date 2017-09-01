9865348bfb91e96b040d7fb20a42d29fc4671ab7
manyce400 <fallon12>
2017-08-31 23:28:40 -0400

    Version 1.0 functionality:  Initial classess and signup for guardian mode.

---
+++ qpalx-elearning-web-zone-content-guardian/src/main/java/com/quaza/solutions/qpalx/elearning/web/zone/content/guardian/registration/GuardianRegistrationController.java
@@ -0,0 +1,15 @@
+package com.quaza.solutions.qpalx.elearning.web.zone.content.guardian.registration;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+/**
+ * @author manyce400
+ */
+@Controller
+@SessionAttributes(value = {"RegistrationSelectionWebVO", "QPalXWebUserVO"})
+public class GuardianRegistrationController {
+
+
+
+}
